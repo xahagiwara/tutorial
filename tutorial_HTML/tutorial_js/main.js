a481ba8ac527113586e74593c88b509f9b08590f
xahagiwara <s.hagiwara@xearts.jp>
2019-04-10 17:56:04 +0900

    add_if

--- tutorial_HTML/tutorial_js/main.js
+++ tutorial_HTML/tutorial_js/main.js
@@ -1,2 +1,10 @@
 var myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello world!';
+
+// === : データ型まで一緒の時にtrueを返す
+var iceCream = 'チョコレート';
+if (iceCream == 'チョコレート') {
+  alert('やった、チョコレートアイス大好き！');    
+} else {
+  alert('あれれ、でもチョコレートは私のお気に入り......');    
+}
