831865539a6bc6566263d54267d8047c9d9d1fad
xearts <xearts@xeartsnoMac-mini.local>
2019-04-11 17:14:20 +0900

    add_if_php

---
+++ tutorial_php/if_php.php
@@ -0,0 +1,12 @@
+<?php
+    //if文のチュートリアル
+    $t = date("H");
+
+    if ($t < "10") {
+        echo "Have a good morning!";
+    } elseif ($t < "20") {
+        echo "Have a good day!";
+    } else {
+        echo "Have a good night!";
+    }
+?>
